5337d1be2923e9a0d4c93d655b01587281022431
ramdhany <nirish777@gmail.com>
2017-05-03 17:43:20 +0100

    show dial app XML

--- www/js/index.js
+++ www/js/index.js
@@ -1,27 +1,29 @@
 
 document.addEventListener('deviceready', function() {
-	var message = document.getElementById("message");
-	message.textContent = "Searching for HbbTVs...";
-
-	var dialClient = DIALClient.getDIALClient();
-	dialClient.startDiscovery(function (terminals) {
-		console.log(terminals);
-
-		var message = document.getElementById("message");
-		var deviceList = document.getElementById("device-list");
-
-		message.textContent = "Found " + terminals.length + " TVs...";
-
-		// Clear device list...
-		while(deviceList.firstChild) {
-			deviceList.removeChild(deviceList.firstChild);
-		}
-
-		// Re-populate with discovered devices
-		for (var terminal of terminals) {
-			var li = document.createElement('li');
-			li.textContent = "" + terminal.friendly_name;
-			deviceList.appendChild(li);
-		}
-	});
-});
+                          var message = document.getElementById("message");
+                          message.textContent = "Searching for HbbTVs...";
+                          
+                          var dialClient = DIALClient.getDIALClient();
+                          dialClient.startDiscovery(function (terminals) {
+                                                    console.log(terminals);
+                                                    
+                                                    var message = document.getElementById("message");
+                                                    var deviceList = document.getElementById("device-list");
+                                                    var appXML = document.getElementById("appXML");
+                                                    
+                                                    message.textContent = "Found " + terminals.length + " TVs...";
+                                                    
+                                                    // Clear device list...
+                                                    while(deviceList.firstChild) {
+                                                    deviceList.removeChild(deviceList.firstChild);
+                                                    }
+                                                    
+                                                    // Re-populate with discovered devices
+                                                    for (var terminal of terminals) {
+                                                    var li = document.createElement('li');
+                                                    li.textContent = "" + terminal.friendly_name;
+                                                    deviceList.appendChild(li);
+                                                    appXML.textContent = terminal.appXML;
+                                                    }
+                                                    });
+                          });
